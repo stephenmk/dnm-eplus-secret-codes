6ac49a02ef946d7ee4261da32d05e0685623542c
stephenmk <stephenmk@users.noreply.github.com>
2025-12-27 16:51:46 -0600

    Add validator for price

--- DnmEplusPassword.Web/Models/Price.cs
+++ DnmEplusPassword.Web/Models/Price.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DnmEplusPassword.Web.Models;
 
 public sealed record Price
 {
+    [Range(1, 999999, ErrorMessage="Price must be between 1 and 999,999")]
     public int Value { get; set; } = 1;
 }
