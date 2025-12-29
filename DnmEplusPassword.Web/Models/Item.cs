7cce9c6a4e3b3767f8aec1b79c43edbc3d1e2b6e
stephenmk <stephenmk@users.noreply.github.com>
2025-12-28 20:17:28 -0600

    Move validation message

--- DnmEplusPassword.Web/Models/Item.cs
+++ DnmEplusPassword.Web/Models/Item.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DnmEplusPassword.Web.Models;
 
 public sealed record Item
 {
+    [StringLength(maximumLength: 4, MinimumLength = 4, ErrorMessage = "Item ID should contain 4 characters.")]
+    [RegularExpression(@"^[0-9A-Fa-f]+$", ErrorMessage = "Item ID should only contain numbers and letters A through F.")]
     public string Id { get; set; } = string.Empty;
 }
