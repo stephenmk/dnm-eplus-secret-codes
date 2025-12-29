c8de34be04f7a691c77729613d7cac032a9a14d2
stephenmk <stephenmk@users.noreply.github.com>
2025-12-28 20:16:52 -0600

    Remove explicit enums

--- DnmEplusPassword.Library/CodeType.cs
+++ DnmEplusPassword.Library/CodeType.cs
@@ -2,12 +2,12 @@ namespace DnmEplusPassword.Library;
 
 public enum CodeType
 {
-    Famicom = 0,     // NES
-    NPC = 1,         // Original NPC Code
-    Card_E = 2,      // NOTE: This can only be sent to villagers in a letter.
-    Magazine = 3,    // Contest?
-    User = 4,        // Player-to-Player
-    Card_E_Mini = 5, // Only one data strip? Hit rate index must be set to 4.
-    New_NPC = 6,     // Using the new password system?
-    Monument = 7,    // Town Decorations (from Object Delivery Service, see: https://www.nintendo.co.jp/ngc/gaej/obje/)
+    Famicom,     // NES
+    NPC,         // Original NPC Code
+    Card_E,      // NOTE: This can only be sent to villagers in a letter.
+    Magazine,    // Contest?
+    User,        // Player-to-Player
+    Card_E_Mini, // Only one data strip? Hit rate index must be set to 4.
+    New_NPC,     // Using the new password system?
+    Monument,    // Town Decorations (from Object Delivery Service, see: https://www.nintendo.co.jp/ngc/gaej/obje/)
 }
