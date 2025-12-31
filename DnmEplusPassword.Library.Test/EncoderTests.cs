68416e75b37526a4781a9219b8933bd69fd5b061
stephenmk <stephenmk@users.noreply.github.com>
2025-12-30 21:10:04 -0600

    Add magazine code test

--- DnmEplusPassword.Library.Test/EncoderTests.cs
+++ DnmEplusPassword.Library.Test/EncoderTests.cs
@@ -52,4 +52,21 @@ public sealed class EncoderTests
         Assert.AreEqual("さふぶゆろじをねぐくよぬがねみみ", password.Item1);
         Assert.AreEqual("ぐべごすねぬげをなづぞほえこひか", password.Item2);
     }
+
+    [TestMethod]
+    public void TestMagazineWarioWoods()
+    {
+        var input = new PasswordInput
+        {
+            CodeType = CodeType.Magazine,
+            RecipientTown = "にんてんどう",
+            Recipient = "ニンテンドウ",
+            Sender = "あいうえお",
+            ItemId = 0x1E20, // ワリオのもり
+            HitRate = HitRate.OneHundredPercent,
+        };
+        var password = Encoder.Encode(input, englishPasswords: false);
+        Assert.AreEqual("すねわとつくずよぞろれはごゆつよ", password.Item1);
+        Assert.AreEqual("へずんすわじぜにのごはせはみへひ", password.Item2);
+    }
 }
