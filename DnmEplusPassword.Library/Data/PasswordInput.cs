a511982328e00ea1099cf35d918535741e72299a
stephenmk <stephenmk@users.noreply.github.com>
2025-12-31 13:57:18 -0600

    Set required properties

--- DnmEplusPassword.Library/Data/PasswordInput.cs
+++ DnmEplusPassword.Library/Data/PasswordInput.cs
@@ -2,13 +2,19 @@ namespace DnmEplusPassword.Library.Data;
 
 public readonly ref struct PasswordInput
 {
-    public readonly CodeType CodeType { get; init; }
-    public readonly ReadOnlySpan<char> RecipientTown { get; init; }
-    public readonly ReadOnlySpan<char> Recipient { get; init; }
-    public readonly ReadOnlySpan<char> Sender { get; init; }
-    public readonly ushort ItemId { get; init; }
+    public required readonly CodeType CodeType { get; init; }
+    public required readonly ReadOnlySpan<char> RecipientTown { get; init; }
+    public required readonly ReadOnlySpan<char> Recipient { get; init; }
+    public required readonly ReadOnlySpan<char> Sender { get; init; }
+    public required readonly ushort ItemId { get; init; }
+
     public readonly byte RowAcre { get; init; }
     public readonly byte ColAcre { get; init; }
+    public readonly int ExtraData => CodeType switch
+    {
+        CodeType.Monument => (RowAcre << 3) | ColAcre,
+        _ => 0
+    };
 
     private readonly HitRate _hitRate;
     public readonly HitRate HitRate
@@ -21,13 +27,7 @@ public readonly ref struct PasswordInput
         init => _hitRate = value;
     }
 
-    public readonly int ExtraData => CodeType switch
-    {
-        CodeType.Monument => (RowAcre << 3) | ColAcre,
-        _ => 0
-    };
-
-    public readonly byte NpcCode => CodeType switch
+    internal readonly byte NpcCode => CodeType switch
     {
         CodeType.NPC or CodeType.New_NPC => 0x00,
         _ => 0xFF
