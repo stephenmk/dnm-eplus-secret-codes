416583010c469fd03640067965a6d2b5849f3e68
stephenmk <stephenmk@users.noreply.github.com>
2025-12-31 00:03:15 -0600

    Add famicom page

---
+++ DnmEplusPassword.Web/Models/FamicomCodeGenerator.cs
@@ -0,0 +1,42 @@
+using DnmEplusPassword.Library;
+
+namespace DnmEplusPassword.Web.Models;
+
+[ValidatableType]
+public sealed class FamicomCodeGenerator
+{
+    public Name RecipientTownName { get; set; } = new() { MaxLength = 6 };
+    public Name RecipientName { get; set; } = new() { MaxLength = 6 };
+    public Name SenderName { get; set; } = new() { MaxLength = 6 };
+    public Item Item { get; set; } = new() { Type = Item.ItemType.Famicom };
+    public Language Language { get; set; } = new();
+
+    public SecretCode? GenerateSecretCode()
+    {
+        if (Item.HexId is not ushort itemId)
+        {
+            return null;
+        }
+        var passwordInput = new PasswordInput
+        {
+            CodeType = CodeType.Famicom,
+            RecipientTown = RecipientTownName.Value,
+            Recipient = RecipientName.Value,
+            Sender = SenderName.Value,
+            ItemId = itemId,
+        };
+        try
+        {
+            var password = Encoder.Encode(passwordInput, Language.IsEnglish);
+            return new()
+            {
+                Line1 = password.Item1,
+                Line2 = password.Item2,
+            };
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
