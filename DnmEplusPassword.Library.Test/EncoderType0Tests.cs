5536f85272853f09ffd8c71ea15752bb4e121eda
stephenmk <stephenmk@users.noreply.github.com>
2025-12-31 00:02:55 -0600

    Add famicom test

---
+++ DnmEplusPassword.Library.Test/EncoderType0Tests.cs
@@ -0,0 +1,21 @@
+namespace DnmEplusPassword.Library.Test;
+
+[TestClass]
+public sealed class EncoderType0Tests
+{
+    [TestMethod]
+    public void TestFamicomPunchout()
+    {
+        var input = new PasswordInput
+        {
+            CodeType = CodeType.Famicom,
+            RecipientTown = "コーヒーまめ",
+            Recipient = "クーパー",
+            Sender = "にんてんどう",
+            ItemId = 0x1E04, // パンチアウト
+        };
+        var password = Encoder.Encode(input, englishPasswords: false);
+        Assert.AreEqual("ぎいのわれのかぬてもさめとさづで", password.Item1);
+        Assert.AreEqual("とことうぶいぶけらぐぶをたりろね", password.Item2);
+    }
+}
