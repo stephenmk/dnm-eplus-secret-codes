9aee6ebb253d6b6239c0f16f3d12238898826157
stephenmk <stephenmk@users.noreply.github.com>
2025-12-31 15:16:17 -0600

    Remove unused methods

--- DnmEplusPassword.Library/RuneCollectionExtensions.cs
+++ DnmEplusPassword.Library/RuneCollectionExtensions.cs
@@ -20,24 +20,4 @@ public static class RuneCollectionExtensions
             }
         });
     }
-
-    public static string FastToString(this Span<Rune> runes)
-        => FastToString((ReadOnlySpan<Rune>)runes);
-
-    public static string FastToString(this ReadOnlySpan<Rune> runes)
-    {
-        int length = 0;
-        foreach (var rune in runes)
-        {
-            length += rune.Utf16SequenceLength;
-        }
-        return string.Create(length, state: runes, static (output, state) =>
-        {
-            int charsWritten = 0;
-            foreach (var rune in state)
-            {
-                charsWritten += rune.EncodeToUtf16(output[charsWritten..]);
-            }
-        });
-    }
 }
