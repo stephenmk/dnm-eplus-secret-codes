2bd173d9e4d78eea48ef6256a111fd5932bb082d
stephenmk <stephenmk@users.noreply.github.com>
2026-01-01 03:26:26 -0600

    Use 0b11 literal instead of 3 literal

--- DnmEplusPassword.Library/Internal/RsaKeyCode.cs
+++ DnmEplusPassword.Library/Internal/RsaKeyCode.cs
@@ -11,22 +11,22 @@ internal readonly ref struct RsaKeyCode
 
     public RsaKeyCode(ReadOnlySpan<byte> data)
     {
-        int bit10 = data[3] & 3;
-        int bit32 = (data[3] >> 2) & 3;
+        int bit10 = data[3] & 0b11;
+        int bit32 = (data[3] >> 2) & 0b11;
 
-        if (bit10 == 3)
+        if (bit10 == 0b11)
         {
-            bit10 = (bit10 ^ bit32) & 3;
-            if (bit10 == 3)
+            bit10 = (bit10 ^ bit32) & 0b11;
+            if (bit10 == 0b11)
             {
                 bit10 = 0;
             }
         }
 
-        if (bit32 == 3)
+        if (bit32 == 0b11)
         {
-            bit32 = (bit10 + 1) & 3;
-            if (bit32 == 3)
+            bit32 = (bit10 + 1) & 0b11;
+            if (bit32 == 0b11)
             {
                 bit32 = 1;
             }
@@ -34,8 +34,8 @@ internal readonly ref struct RsaKeyCode
 
         if (bit10 == bit32)
         {
-            bit32 = (bit10 + 1) & 3;
-            if (bit32 == 3)
+            bit32 = (bit10 + 1) & 0b11;
+            if (bit32 == 0b11)
             {
                 bit32 = 1;
             }
