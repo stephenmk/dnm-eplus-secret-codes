4d9a621b8b1623b2544029b2a4a9592b0a16ddf9
stephenmk <stephenmk@users.noreply.github.com>
2026-01-01 13:09:57 -0600

    Add monument test

--- DnmEplusPassword.Library.Test/EncoderTests/EncoderType7Tests.cs
+++ DnmEplusPassword.Library.Test/EncoderTests/EncoderType7Tests.cs
@@ -22,4 +22,22 @@ public sealed class EncoderType7Tests
         Assert.AreEqual("すまぜんふねちそそこずぎやむぐら", password.Item1);
         Assert.AreEqual("みなまえざほにぎごべぢぜびけもじ", password.Item2);
     }
+
+    [TestMethod]
+    public void TestMonumentStonehenge()
+    {
+        var input = new PasswordInput
+        {
+            CodeType = CodeType.Monument,
+            RecipientTown = "コーヒーまめ",
+            Recipient = "クーパー",
+            Sender = "1234", // Price
+            ItemId = (ushort)Monument.Stonehenge,
+            RowAcre = 3,
+            ColAcre = 3,
+        };
+        var password = Encoder.Encode(input, englishPasswords: false);
+        Assert.AreEqual("ゆれびのほそかぜれくごびしおげあ", password.Item1);
+        Assert.AreEqual("いぎきほしみやほようんほとづぶか", password.Item2);
+    }
 }
