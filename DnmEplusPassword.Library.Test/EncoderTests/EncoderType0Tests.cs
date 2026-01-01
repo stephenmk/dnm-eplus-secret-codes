68393252aa5bfcdc4248f218527ac53cb196494f
stephenmk <stephenmk@users.noreply.github.com>
2026-01-01 12:14:00 -0600

    Add famicom test

--- DnmEplusPassword.Library.Test/EncoderTests/EncoderType0Tests.cs
+++ DnmEplusPassword.Library.Test/EncoderTests/EncoderType0Tests.cs
@@ -20,4 +20,20 @@ public sealed class EncoderType0Tests
         Assert.AreEqual("ぎいのわれのかぬてもさめとさづで", password.Item1);
         Assert.AreEqual("とことうぶいぶけらぐぶをたりろね", password.Item2);
     }
+
+    [TestMethod]
+    public void TestFamicomCluCluLand()
+    {
+        var input = new PasswordInput
+        {
+            CodeType = CodeType.Famicom,
+            RecipientTown = "コーヒーまめ",
+            Recipient = "クーパー",
+            Sender = string.Empty,
+            ItemId = 0x1E0C, // クルクルランドＤ
+        };
+        var password = Encoder.Encode(input, englishPasswords: false);
+        Assert.AreEqual("ぶななむううわすはとうそらりぎぐ", password.Item1);
+        Assert.AreEqual("くめぶびべらろそむはたぶぐこださ", password.Item2);
+    }
 }
